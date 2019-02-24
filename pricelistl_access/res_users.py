715e76ad3ce038c4a80556c3371be96cadcb19d7
Badrelden Ahmed <badreldenbeko@users.noreply.github.com>
2019-02-24 19:41:00 +0200

    remove apps from branch 11.0

--- pricelistl_access/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields,api
-
-class users(models.Model):
-    _name = 'res.users'
-    _inherit = 'res.users'
-    pricelist_ids = fields.Many2many('product.pricelist', 'pricelist_security_pricelist_users','user_id','pricelist_id', 'Restricted Pricelists', help="This Pricelists and the information related to it will be only visible for users where you specify that they can see them setting this same field.")
-    
